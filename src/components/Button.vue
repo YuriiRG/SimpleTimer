394b6865ea3de7045af8ee88a6ff8b417b789138
YuriiRG <yuriirg@tutanota.com>
2022-08-27 13:43:13 +0200

    Small button UI improvment

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -18,17 +18,18 @@ const emit = defineEmits<{
     background-color: #fff;
     border: none;
     padding: 0.6em;
-    font-size: 16px;
+    font-size: inherit; 
     border-radius: 0.5em;
     box-shadow: 0.1rem 0.1rem 1rem #aaa;
     transition: background-color 0.15s;
   }
   .button:hover {
     background-color: #eee;
-    transition: background-color 0.15s;
+    transition: background-color 0.15s, transform 0.05s;
   }
 
-  .button:active {
-    background-color: #dfdfdf;
+  .button:hover:active {
+    background-color: #e5e5e5;
+    transform: scale(0.95);
   }
 </style>
