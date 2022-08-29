532486ab7ebb8a3c6925c91178919830fcd21dd2
YuriiRG <yuriirg@tutanota.com>
2022-08-29 16:23:46 +0200

    Create basic timer UI

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,10 +1,36 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const percent = ref(123);
 </script>
 
 <template>
-
+  <div class="timer">
+    <div class="text">
+      {{percent}}
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
+.timer {
+  width: 10em;
+  height: 10em;
+  position: relative;
+  border-radius: 50%;
+  background: conic-gradient(green v-bind('`${percent}deg`'), lightgrey v-bind('`${percent}deg`') 360deg);
+}
+.text {
+  --timer-line-width: 10%;
+  position: absolute;
+  display: flex;
+  width: calc(100% - 2*var(--timer-line-width));
+  height: calc(100% - 2*var(--timer-line-width));
+  top: var(--timer-line-width);
+  left: var(--timer-line-width);
+  align-items: center;
+  justify-content: center;
+  background: #fff;
+  border-radius: 50%;
+}
 </style>
