13179c52dfe4a6aa0a66160b3519b98fcc6be3d2
YuriiRG <yuriirg@tutanota.com>
2022-09-21 15:18:55 +0200

    Improve locale-choosing algorithm

--- src/main.ts
+++ src/main.ts
@@ -10,14 +10,20 @@ let locale = "en";
 if (localStorage.getItem("lang")) {
   locale = localStorage.getItem("lang")!;
 } else {
-  // TODO: replace with a smarter algorithm,
-  // which should check navigator.languages
-  // also probably move to separate file
-  ["en", "ru"].forEach((lang) => {
-    if (navigator.language.startsWith(lang)) {
-      locale = lang;
+  let availableLangs = ["ru", "en"];
+  let isFound = false;
+  for (let i = 0; i < navigator.languages.length; i++) {
+    for (let j = 0; j < availableLangs.length; j++) {
+      if (navigator.languages[i].startsWith(availableLangs[j])) {
+        locale = availableLangs[j];
+        isFound = true;
+        break;
+      }
     }
-  });
+    if (isFound) {
+      break;
+    }
+  }
 }
 
 const i18n = createI18n({
