a299e3e2bfea90c81fa9ad90e2f5f86268ec21ee
YuriiRG <yuriirg@tutanota.com>
2022-09-23 18:21:51 +0200

    Add typical usage test

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -1,6 +1,26 @@
-describe("empty spec", () => {
-  it("passes", () => {
+describe("Typical usage", () => {
+  before(() => {
+    cy.clearLocalStorage();
+  });
+  beforeEach(() => {
     cy.visit("http://127.0.0.1:5173/SimpleTimer/");
-    expect(true).to.equal(true);
+    cy.get(".locale-changer select").select("en");
+  });
+  it("Typical usage", () => {
+    cy.contains("00:00:00").click();
+    cy.get(".time-input").last().clear().type("1");
+    cy.contains("Close").click();
+    cy.contains("Start").click();
+    cy.wait(100);
+    cy.contains("Pause").click();
+    cy.wait(500);
+    cy.contains("Start").click();
+    cy.wait(700);
+    cy.contains("00:00:01");
+    cy.contains("Pause");
+    cy.wait(250);
+    cy.contains("00:00:00");
+    cy.contains("Reset").click();
+    cy.contains("00:00:01");
   });
 });
